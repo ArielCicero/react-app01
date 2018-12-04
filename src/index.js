74b1b2ec3f51baf296ddbfa88be7cf84f7582df7
Ariel Cicero <ariel.cicero@gmail.com>
2018-12-04 14:42:44 +0100

    starting learning process

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = <h1>Hellow World</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(element, document.getElementById("root"));
+/*
+const person = {
+  name: "Ariel",
+  getPersonObject() {
+    console.log(this);
+  }
+};
+
+console.log(person.name);
+
+const targetMember = "name";
+console.log(person[targetMember]);
+
+person.getPersonObject();
+console.log(person.getPersonObject);
+
+const x = person.getPersonObject;
+console.log(x);
+x();
+const getPersonObject = person.getPersonObject.bind(person);
+getPersonObject();
+*/
+
+//## Arrow functions:
